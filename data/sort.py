92885631dcac41ae25d8423f50fe7e2d08d810e6
Vaibhav Agrawal <vb8817@gmail.com>
2018-07-01 10:34:13 +0530

    Update sort.py

--- data/sort.py
+++ data/sort.py
@@ -4,21 +4,6 @@ import glob
 import os
 count = 0
 orig_stdout = sys.stdout
-# read_files = glob.glob("split_placeID/*.txt")
-# for f in read_files:
-# with open(f, 'r', encoding="utf-8") as myfile:
-# 	base=os.path.basename(f)    
-# 	basename=os.path.splitext(base)[0]
-# 	content = myfile.readlines()
-# 	content = [x.strip() for x in content]
-# 	for x in content:
-# 		count+=1
-# 		basename.strip()
-# 		x.strip()
-# 		sys.stdout = open("total_placeId.txt","a")
-# 		print(basename,",",x,sep="")
-# 		sys.stdout.close()
-# 		sys.stdout = orig_stdout
 with open("total_placeId.txt", 'r') as file:
 	sorted_data=sorted(file.readlines(), 
 					   key=lambda item: int(item.rsplit(',',1)[-1].strip()))
